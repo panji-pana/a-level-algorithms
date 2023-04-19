8ac32dc5f3872e93429f9c11ccf803248b147bd9
panji-pana <isabellewestcott24@gmail.com>
2023-04-19 11:57:34 +0100

    Added 4/5 BST algorithms

--- python/binary_search_tree.py
+++ python/binary_search_tree.py
@@ -29,3 +29,62 @@ class BinarySearchTree(Node):
                     else:
                         current_node = current_node.right
                         
+    def preorder(self, node = None, aim = None):
+        if node == None:
+            node = self.root 
+        if node == aim:
+            return node
+            
+        print(node.data)
+        if node.left != None:
+            self.preorder(node.left)
+        if node.right != None:
+            self.preorder(node.right)
+            
+    def inorder(self, node = None, aim=None):      
+        if node == None:
+            node = self.root
+        if node == aim:
+            return node
+            
+        if node.left != None:
+            self.inorder(node.left)
+        print(node.data)
+        if node.right != None:
+            self.inorder(node.right)
+    
+    def postorder(self, node = None, aim=None):      
+        if node == None:
+            node = self.root
+        if node == aim:
+            return node
+            
+        if node.left != None:
+            self.postorder(node.left)
+        if node.right != None:
+            self.postorder(node.right) 
+        print(node.data) 
+ 
+ 
+        
+    
+if __name__ == "__main__":
+    bst = BinarySearchTree()
+    bst.insert(50)
+    bst.insert(30)
+    bst.insert(20)
+    bst.insert(40)
+    bst.insert(70)
+    bst.insert(60)
+    bst.insert(80)
+    bst.insert(10)
+
+    print("Preorder pre-delete")
+    bst.preorder()
+    print("\nInorder pre-delete")
+    bst.inorder()
+    
+    
+    
+    
+
